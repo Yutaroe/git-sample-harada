1a2c4d75098684189006e7a8a401dedb69930b62
yutaroe <yucl895@gmail.com>
2021-08-03 17:37:03 +0900

    さらに追加しましたHelloWorldに

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <div>追加しました</div>
+    <div>また追加しました</div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
